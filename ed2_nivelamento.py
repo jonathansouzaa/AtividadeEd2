ad647d8f9329db9e4e1e39b2452a50a0ed764ea6
Rafael Ventura <raf4a@DESKTOP-5HNLOS1.localdomain>
2022-10-08 21:29:29 -0300

    add questao 8 python

--- ed2_nivelamento.py
+++ ed2_nivelamento.py
@@ -215,17 +215,18 @@ while (rodar):
 ### Questão 8
 #Apesar de não ser o algoritmo mais rápido, o bubble sort funciona muito bem para quantidade média de itens a serem ordenados 
 from heapq import heappop, heappush
-def ordenacaoHeap(array):
-    heap = []
-    for element in array:
-        heappush(heap, element)
 
-    ordered = []
+def ordenarRemedios(listaRemedios):
+    heap = []
+    for remedio in listaRemedios:
+        heappush(heap, remedio)
+
+    remediosOrdenados = []
 
     while heap:
-        ordered.append(heappop(heap))
+        remediosOrdenados.append(heappop(heap))
 
-    return ordered
+    return remediosOrdenados
 
 array = [13, 21, 15, 5, 26, 4, 17, 18, 24, 2]
-print(ordenacaoHeap(array))
+print(ordenarRemedios(array))
